899855262b76571c805b89ea3c38cff4cc1fea11
Hamza Irfan <Hamza1AtWork@gmail.com>
2024-02-25 23:05:42 +0500

    Hamza's commit

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import WebcamComponent from './component/WebcamComponent';
+import WebcamComponent from "./component/WebcamComponent";
 const App = () => {
-
   return (
-    <div><WebcamComponent /></div>
-
+    <div>
+      <WebcamComponent />
+    </div>
   );
 };
 
 export default App;
-
